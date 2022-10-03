07fa8ce609f73155c9f12d6602650263ec1f4fb2
Емельян Егоров <emelya.kara@yandex.ru>
2022-10-03 19:14:46 +0500

    вроде работает

--- Задача 5/Задача 5.cpp
+++ Задача 5/Задача 5.cpp
@@ -1,14 +1,30 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 
 int main()
 {
-    int x,i = 0;
-    long double f = 0;
-    cin >> x;
-    while (true)
+    setlocale(LC_ALL, "Russian");
+
+    long double f = 0, x, sum = 0, xn = 0, e;
+    int n = 0;
+    
+    do
     {
-        f += 
+        cout << "введите x (|x| < 1) и e (эпсилон)\n";    // проверка ввода
+        cin >> x >> e;
+    } while (abs(x) >= 1 || e <= 0);         
+    
+    xn = x;
+    sum += xn;
+    
+    while (abs(sum) < e) // цикл по поиску f(x)
+    {
+        n += 1;
+        sum += xn * ((((-1) * ((2 * n - 1) * (2 * n - 1))) / (2 * n)) * (x * x)); // (n-1) + n член последовательности
+        xn = xn * ((((-1) * ((2 * n - 1) * (2 * n - 1))) / (2 * n)) * (x * x)); // (n-1)-ый член последовательности
     }
+    
+    cout << "моя программа: " << sum << "\nфункция из интернета: " << log(x + sqrt(x * x + 1));
 }
